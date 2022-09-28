cebe79b625539717f4a38bf4f0f1f70d04398a97
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-28 17:40:36 +0800

    feat: play animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,44 @@ import gsap from 'gsap';
 
 function App() {
   const el = React.useRef();
+  const [isPlaying, setIsPlaying] = React.useState(false);
   const q = gsap.utils.selector(el);
+  const tlPlay = React.useRef();
+
+  const handleClick = () => {
+    if (!isPlaying) {
+      tlPlay.current.resume();
+      setIsPlaying(true);
+    } else {
+      tlPlay.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  React.useEffect(() => {
+    gsap.set(q('.disk'), { transformOrigin: '409px 497px', x: 40 });
+    gsap.set(q('.bar'), { transformOrigin: '834px 182px', y: 110, x: -120 });
+
+    tlPlay.current = gsap
+      .timeline()
+      .to(q('.bar'), { duration: 1.5, rotation: 30 })
+      .to(q('.disk'), {
+        duration: 2,
+        rotation: 360,
+        repeat: -1,
+        ease: 'linear',
+      })
+      .pause();
+  }, []);
 
   return (
     <div className='App' ref={el}>
       <img src='/background.svg' alt='background' className='svg background' />
       <img src='/disk.svg' alt='disk' className='svg disk' />
       <img src='/bar.svg' alt='bar' className='svg bar' />
+      <button onClick={handleClick} className='btn'>
+        Play
+      </button>
     </div>
   );
 }
