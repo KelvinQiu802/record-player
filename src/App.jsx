226d20d1a2966596e42c01b2daaeb33e244ab976
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-28 16:18:37 +0800

    feat: import svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
+import React from 'react';
+import gsap from 'gsap';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  const el = React.useRef();
+  const q = gsap.utils.selector(el);
+
+  return (
+    <div className='App' ref={el}>
+      <img src='/background.svg' alt='background' className='svg background' />
+      <img src='/disk.svg' alt='disk' className='svg disk' />
+      <img src='/bar.svg' alt='bar' className='svg bar' />
+    </div>
+  );
 }
 
 export default App;
